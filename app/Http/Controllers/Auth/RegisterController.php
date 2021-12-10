f6e7f4f8c4ab5be3a0c4b4e2344164dd56ec350e
khohoh <ianjeong0820@gmail.com>
2021-12-09 22:03:04 -0800

    many changes added

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -39,6 +39,6 @@ class RegisterController extends Controller
 
         auth()->attempt($request->only('email', 'password'));
 
-        return redirect()->route('dashboard');
+        return redirect()->route('posts');
     }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,19 +8,21 @@
                 <div>Join us to post what you want to share with people for free!</div>
             </div>
 
-            <div class="flex justify-center mt-7">
-                <div>
-                    <a href="{{ route('login') }}" 
-                            class="w-32 border-2 border-blue-500  text-blue-500 ml-3 px-2 py-1 rounded font-medium hover:bg-blue-500 hover:text-white">
-                            Login</a>
+            @if(!auth()->user())           
+                <div class="flex justify-center mt-7">
+                    <div>
+                        <a href="{{ route('login') }}" 
+                                class="w-32 border-2 border-blue-500  text-blue-500 ml-3 px-2 py-1 rounded font-medium hover:bg-blue-500 hover:text-white">
+                                Login</a>
+                    </div>
+            
+                    <div>
+                        <a href="{{ route('register') }}" 
+                                class="w-32 border-2 border-blue-500  text-blue-500 ml-3 px-2 py-1 rounded font-medium hover:bg-blue-500 hover:text-white">
+                                Register</a>
+                    </div>
                 </div>
-        
-                <div>
-                    <a href="{{ route('register') }}" 
-                            class="w-32 border-2 border-blue-500  text-blue-500 ml-3 px-2 py-1 rounded font-medium hover:bg-blue-500 hover:text-white">
-                            Register</a>
-                </div>
-            </div>
+            @endif
         </div>
 
         
