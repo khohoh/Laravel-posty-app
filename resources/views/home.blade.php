a685c7e7f2568cb63b486242de7174b0504dd81c
khohoh <ianjeong0820@gmail.com>
2021-12-09 21:42:19 -0800

    many changes added

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,9 +2,27 @@
 
 @section('content')
     <div class="flex justify-center">
-        {{-- <div class="w-8/12  bg-white p-6 rounded-lg"> --}}
-        <div style="background-image: url('https://cdn.pixabay.com/photo/2020/08/03/02/30/hello-5458998_960_720.png')"
-             class="mt-0 w-6/12 h-screen bg-no-repeat rounded-lg">            
+        <div class="w-6/12 bg-white p-6 rounded-lg mt-40">
+            <div class="grid grid-row-2 justify-center gap-3 text-center">
+                <div class="text-3xl">Welcome To Posty App</div>
+                <div>Join us to post what you want to share with people for free!</div>
+            </div>
+
+            <div class="flex justify-center mt-7">
+                <div>
+                    <a href="{{ route('login') }}" 
+                            class="w-32 border-2 border-blue-500  text-blue-500 ml-3 px-2 py-1 rounded font-medium hover:bg-blue-500 hover:text-white">
+                            Login</a>
+                </div>
+        
+                <div>
+                    <a href="{{ route('register') }}" 
+                            class="w-32 border-2 border-blue-500  text-blue-500 ml-3 px-2 py-1 rounded font-medium hover:bg-blue-500 hover:text-white">
+                            Register</a>
+                </div>
+            </div>
         </div>
+
+        
     </div>
 @endsection
